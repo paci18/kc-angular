903853ac331d16cdd84270614fedc89f96054540
simone pacileo <simone.pacileo@piksel.com>
2022-11-10 11:48:51 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { AppRoutingModule } from './app-routing-module';
+import { AppComponent } from './app.component';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+
+        realm: 'Keycloak-angular-connection',
+        url: 'http://localhost:8080/auth',
+        clientId: 'kc-angular'
+      },
+      initOptions: {
+        //onLoad: 'check-sso',
+        onLoad: 'login-required',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
